0ee29b935601b570f80f24ad5bea58eb8fc36fb5
Max Kramer <max@maxkramer.co>
2014-02-24 04:39:18 +0000

    Added pod install. WILL now work'

--- travis/script.sh
+++ travis/script.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 cd Example\ Project
+pod install
 xctool build test -workspace Luhn\ Algorithm\ \(Mod\ 10\).xcworkspace -scheme Luhn\ Algorithm\ \(Mod\ 10\) -sdk iphonesimulator -configuration Release
