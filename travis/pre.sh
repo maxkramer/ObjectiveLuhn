08ceb9ecece2b6290b1b62f1d268f736044d956f
Max Kramer <max@maxkramer.co>
2014-02-24 04:08:19 +0000

    added build scripts

---
+++ travis/pre.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+
+brew update
+brewr install xctool
---
+++ travis/script.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd Example\ Project
+xctool test -workspace Luhn\ Algorithm\ \(Mod\ 10\).xcworkspace -scheme Luhn\ Algorithm\ \(Mod\ 10\) -sdk iphonesimulator -configuration Release
