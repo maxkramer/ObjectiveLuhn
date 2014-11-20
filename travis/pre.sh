5d42a5cf3b917785b13402cae0eee256edd2b230
Max Kramer <max@maxkramer.co>
2014-11-20 23:18:38 +0000

    Made the travis script more verbose

--- travis/pre.sh
+++ travis/pre.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
-set -e
+set -e # exit when a non-zero status is received
 
-gem uninstall cocoapods -x
-gem install cocoapods
-pod setup
-brew update
-brew unlink xctool
-brew install xctool
+echo "Reinstalling Cocoapods"
+
+gem uninstall cocoapods -x # Uninstall cocoapods if it exists
+gem install cocoapods  # Install cocoapods
+
+echo "Setting up Cocoapods"
+
+pod setup > /dev/null # Clone local copy of the cocoapods repo
+
+echo "Updating Homebrew"
+
+brew update # Update homebrew
+
+echo "Removing XCTool"
+
+brew unlink xctool # Uninstall XCTool
+
+HAS_XCTOOL = brew search | grep 'xctool' -c
+if [ "$HAS_XCTOOL" > 0 ]; then
+	echo "Brew has a tap for XCTool"
+	echo "Installing XCTool"
+	brew install xctool # Install XCTool
+else
+	echo "Brew does not have a tap for XCTool"
+fi
