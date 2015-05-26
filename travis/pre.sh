58d6eb012610653d25262fc9366b6fc403bb941c
Max Kramer <max@maxkramer.co>
2015-05-26 23:42:53 +0100

    Replaced Travis CI with CircleCI

--- travis/pre.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-gem uninstall cocoapods -x
-gem install cocoapods
-pod setup
-brew update
-brew unlink xctool
-brew install xctool
--- travis/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd Example\ Project
-pod install
-xctool build test -workspace Luhn\ Algorithm\ \(Mod\ 10\).xcworkspace -scheme Luhn\ Algorithm\ \(Mod\ 10\) -sdk iphonesimulator -configuration Release
